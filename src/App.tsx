b572517c967231e78bbb7e922c912af40654cb1f
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-07 17:46:12 +0100

    :fire: Setup: Folder structure setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
+import NotFound from './pages/NotFound';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/auth" element={<Auth />} />
+      {/* <Route path="/auth" element={<Auth />} />
+      {routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            element={
+              <ProtectedtRoutes>
+                <route.layout>
+                  <route.component />
+                </route.layout>
+              </ProtectedtRoutes>
+            }
+          />
+        );
+      })} */}
+    </Routes>
   );
 }
 
