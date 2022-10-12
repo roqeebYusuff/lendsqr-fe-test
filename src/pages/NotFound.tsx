a713576df0888f546bf130d04f9717eadde12635
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-12 22:12:04 +0100

    :zap: 404: Added 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const NotFound = () => {
+const NotFound: React.FC = () => {
+  const navigate = useNavigate()
   return (
-    <div>NotFound</div>
+    <section className="min-vh-100 d-flex justify-content-center align-items-center">
+      <div className="not-found">
+        <h2>404</h2>
+        <h5>Page Not Found</h5>
+        <p>Sorry, we can't find the page you are looking for.</p>
+        <button className="" onClick={() => navigate(-1)}>
+          Go back
+        </button>
+      </div>
+    </section>
   )
 }
 
