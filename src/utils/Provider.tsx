1773dc9429fe0e3b97d6abad1ebafebbd11c2c2e
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-12 10:59:27 +0100

    :fire: Provider: Provider setup complete

---
+++ src/utils/Provider.tsx
@@ -0,0 +1,134 @@
+import { createContext, useContext, useState, useCallback, useMemo, useEffect } from 'react'
+
+export const LendsqrUserContext: React.Context<null | any> = createContext(null)
+const LOCAL_STORAGE_NAME = 'lendsqruser'
+
+interface Props {
+    children?: React.ReactNode
+}
+
+export const useAuth: any = () => {
+    const authContext = useContext(LendsqrUserContext)
+
+    if (authContext === null) {
+        throw new Error(
+            'useAuth() can only be used inside of <LendsqrProvider />, ' +
+            'please declare it at a higher level.'
+        )
+    }
+
+    const { user } = authContext
+
+    return useMemo(
+        () => ({
+            ...user
+        }),
+        [user]
+    )
+}
+
+
+const LendsqrProvider: React.FC<Props> = ({ children }) => {
+    const userContext = useContext(LendsqrUserContext)
+
+    if (userContext !== null) {
+        throw new Error('LendsqrProvider has lready been declared')
+    }
+
+
+    const [authUser, setAuthUser] = useState<[] | any>(null)
+    const [initialized, setInitialized] = useState<boolean>(false)
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+    const [showSidebar, setShowSidebar] = useState<boolean>(false)
+
+    const login = useCallback(
+        (email: string, password: string) => {
+            return new Promise((resolve, reject) => {
+                localStorage.setItem(LOCAL_STORAGE_NAME, email)
+                setAuthUser({ email, password })
+                setIsLoggedIn(true)
+                resolve({ success: true })
+            });
+        },
+        [isLoggedIn, userContext],
+    )
+
+    const logout = useCallback(() => {
+        localStorage.removeItem(LOCAL_STORAGE_NAME)
+        setIsLoggedIn(false)
+    }, [userContext, isLoggedIn])
+
+    const fetchUsers = useCallback(() => {
+        setInitialized(true)
+        return new Promise((resolve, reject) => {
+            fetch('http://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users', {
+                "method": "GET"
+            })
+                .then(response => {
+                    if (response.status !== 200) throw new Error();
+                    return response.json();
+                })
+                .then(json => {
+                    resolve(json)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }, [])
+
+    const userDetails = useCallback((id: number) => {
+        return new Promise((resolve, reject) => {
+            fetch(`http://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users/${id}`, {
+                "method": "GET"
+            })
+                .then(response => {
+                    if (response.status !== 200) throw new Error();
+                    return response.json();
+                })
+                .then(json => {
+                    resolve(json)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }, [])
+
+
+    const toggleSidebar = useCallback(() => {
+        console.log('running herer')
+        setShowSidebar(!showSidebar)
+    }, [showSidebar])
+
+    /* Check if users credentials are stored */
+    useEffect(() => {
+        if (localStorage.getItem(LOCAL_STORAGE_NAME)) {
+            setIsLoggedIn(true)
+        }
+        else {
+            setIsLoggedIn(false)
+        }
+
+        return () => { }
+    }, [userContext, isLoggedIn])
+
+    let users = useMemo(() => ({
+        authUser,
+        login,
+        logout,
+        fetchUsers,
+        userDetails,
+        isLoggedIn,
+        toggleSidebar,
+        showSidebar
+    }), [login, fetchUsers, userDetails, isLoggedIn, authUser, logout, toggleSidebar, showSidebar])
+
+    return (
+        <LendsqrUserContext.Provider value={{ user: users }}>
+            {children}
+        </LendsqrUserContext.Provider>
+    )
+}
+
+export default LendsqrProvider
