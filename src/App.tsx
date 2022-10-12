9a96d31cfbe9fb2dfeacedb9367b91f9f52c9c49
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-12 10:54:40 +0100

    :art: Routes: Modified Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
+import { LendsqrUserContext } from './utils/Provider';
 
-function App() {
-  let routess = useRoutes(routes)
+const App: React.FC = () => {
+  const store = useContext(LendsqrUserContext)
+  let isLoggedIn = store.user.isLoggedIn
+  let routess = useRoutes(routes(isLoggedIn))
+
   return (
     <>
       {routess}
