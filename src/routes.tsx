a14949f9c3855ffac5aadd7599e04f04f858c2b5
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-12 10:52:56 +0100

    :art: Routes: Modiifed Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,25 +1,34 @@
-import { Navigate, RouteObject } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import DashboardLayout from './layouts/DashboardLayout';
+import MainLayout from "./layouts/MainLayout";
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
+import GeneralDetails from "./pages/users/GeneralDetails";
 
 
-const routes: RouteObject[] = [
-  {
-    path: '/',
-    element: <Navigate to="/dashboard" replace />
-  },
-  { path: "auth", element: <Auth /> },
+const routes = (isLoggedIn: boolean) => [
   { path: "*", element: <NotFound /> },
   {
-    path: "/dashboard",
-    element: <DashboardLayout />,
+    path: '/',
+    element: !isLoggedIn ? <MainLayout /> : <Navigate to="/users" />,
     children: [
       {
-        path: "",
-        element: <Dashboard />,
+        path: 'auth',
+        element: <Auth />
       },
+      { path: '/', element: <Navigate to="/auth" /> },
+    ]
+  },
+  {
+    path: 'users',
+    element: isLoggedIn ? <DashboardLayout /> : <Navigate to='/auth' />,
+    children: [
+      {
+        path: 'details',
+        element: <GeneralDetails />
+      },
+      { path: '', element: <Dashboard /> },
     ],
   },
 ]
