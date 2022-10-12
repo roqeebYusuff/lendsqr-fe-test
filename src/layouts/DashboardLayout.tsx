0607b25efcc3cdfb103fae3c8712e38150d711f0
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-12 11:08:47 +0100

    :memo: Layout: Mod dahboard layout with styles

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import { LendsqrUserContext } from '../utils/Provider';
 
 interface Props {
     children?: React.ReactNode
 }
 
 const DashboardLayout: React.FC<Props> = ({ children }) => {
+    const { showSidebar } = useContext(LendsqrUserContext).user
     return (
-        <main className='main'>
+        <section className='main'>
             <Navbar />
-            {children || <Outlet />}
-        </main>
+            <div className="main-wrapper">
+                <aside className={`sidebar ${showSidebar ? 'showSidebar' : 'hideSidebar'}`}>
+                    <Sidebar />
+                </aside>
+                <main>
+                    {children || <Outlet />}
+                </main>
+            </div>
+        </section>
     )
 }
 
