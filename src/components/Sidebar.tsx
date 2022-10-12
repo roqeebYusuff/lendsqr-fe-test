1d8ff02581efe1e10a1a3ad4b37f6210ffe74c9d
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-10-12 21:44:27 +0100

    :memo: Sidebar: Mod sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React from 'react'
 import { Customers, Businesses, Settings } from '../data/SidebarItems'
 import briefcase from '../assets/icons/briefcase.svg';
 import home from '../assets/icons/home.svg';
+import signout from '../assets/icons/sign-out 1.svg';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../utils/Provider';
 
 const Sidebar: React.FC = () => {
+    const { logout } = useAuth()
     return (
         <div className={`sidebar-container py-4 `}>
             <div className="sidebar-menu">
@@ -78,8 +81,8 @@ const Sidebar: React.FC = () => {
                 <div className="sidebar-menu">
                     <ul className='menu-container'>
                         <li className='menu-item'>
-                            <NavLink to='/logout' className={({ isActive }) => isActive ? 'active' : ''}>
-                                <img src={home} alt="Home" />
+                            <NavLink to='/logout' onClick={(e) => { e.preventDefault(); logout() }} className={({ isActive }) => isActive ? 'active' : ''}>
+                                <img src={signout} alt="Home" />
                                 Logout
                             </NavLink>
                         </li>
--- src/data/SidebarItems.ts
+++ src/data/SidebarItems.ts
@@ -1,4 +1,23 @@
 import usersIcon from '../assets/icons/users.svg';
+import guarantorsIcon from '../assets/icons/users 1.svg';
+import loansIcon from '../assets/icons/sack 1.svg';
+import decisionsIcon from '../assets/icons/handshake-regular 1.svg';
+import savingsIcon from '../assets/icons/piggy-bank 1.svg';
+import loanReqIcon from '../assets/icons/Group 104.svg';
+import whitelistIcon from '../assets/icons/user-check 1.svg';
+import karmaIcon from '../assets/icons/user-times 1.svg';
+import orgIcon from '../assets/icons/briefcase.svg';
+import savingsProductIcon from '../assets/icons/np_bank_148501_000000 1.svg';
+import sysmessagesIcon from '../assets/icons/tire 1.svg';
+import feesAndChargesIcon from '../assets/icons/coins-solid 1.svg'
+import transactionsIcon from '../assets/icons/icon.svg'
+import servicesIcon from '../assets/icons/galaxy 1.svg'
+import serviceAndAccIcon from '../assets/icons/user-cog 1.svg'
+import settlementIcon from '../assets/icons/scroll 1.svg'
+import reportsIcon from '../assets/icons/chart-bar 2.svg'
+import preferencesIcon from '../assets/icons/sliders-h 1.svg'
+import logsIcon from '../assets/icons/clipboard-list 1.svg'
+import feesAndPricingIcon from '../assets/icons/badge-percent 1.svg'
 
 interface Props {
     title: string,
@@ -15,37 +34,37 @@ export const Customers: Props[] = [
     {
         title: "Guarantors",
         to: "/guarantors",
-        icon: usersIcon
+        icon: guarantorsIcon
     },
     {
         title: "Loans",
         to: "/loans",
-        icon: usersIcon
+        icon: loansIcon
     },
     {
         title: "Decision Models",
         to: "/decision-models",
-        icon: usersIcon
+        icon: decisionsIcon
     },
     {
         title: "Savings",
         to: "/savings",
-        icon: usersIcon
+        icon: savingsIcon
     },
     {
         title: "Loan Requests",
         to: "/loan-requests",
-        icon: usersIcon
+        icon: loanReqIcon
     },
     {
         title: "Whitelist",
         to: "/whitelist",
-        icon: usersIcon
+        icon: whitelistIcon
     },
     {
         title: "Karma",
         to: "/karma",
-        icon: usersIcon
+        icon: karmaIcon
     },
 ]
 
@@ -53,47 +72,47 @@ export const Businesses: Props[] = [
     {
         title: "Organization",
         to: "/organization",
-        icon: usersIcon
+        icon: orgIcon
     },
     {
         title: "Loan Products",
         to: "/loan-products",
-        icon: usersIcon
+        icon: loanReqIcon
     },
     {
         title: "Savings Products",
         to: "/loans",
-        icon: usersIcon
+        icon: savingsProductIcon
     },
     {
         title: "Fees and Charges",
         to: "/fees-and-charges",
-        icon: usersIcon
+        icon: feesAndChargesIcon
     },
     {
         title: "Transactions",
         to: "/transactions",
-        icon: usersIcon
+        icon: transactionsIcon
     },
     {
         title: "Services",
         to: "/services",
-        icon: usersIcon
+        icon: servicesIcon
     },
     {
         title: "Service Account",
         to: "/service-account",
-        icon: usersIcon
+        icon: serviceAndAccIcon
     },
     {
         title: "Settlements",
         to: "/settlements",
-        icon: usersIcon
+        icon: settlementIcon
     },
     {
         title: "Reports",
         to: "/reports",
-        icon: usersIcon
+        icon: reportsIcon
     },
 ]
 
@@ -101,21 +120,21 @@ export const Settings: Props[] = [
     {
         title: "Preferences",
         to: "/preferences",
-        icon: usersIcon
+        icon: preferencesIcon
     },
     {
         title: "Fees and Pricing",
         to: "/fees-and-pricing",
-        icon: usersIcon
+        icon: feesAndPricingIcon
     },
     {
         title: "Audit Logs",
         to: "/audit-logs",
-        icon: usersIcon
+        icon: logsIcon
     },
     {
         title: "Systems Messages",
         to: "/systems-messages",
-        icon: usersIcon
+        icon: sysmessagesIcon
     },
 ]
